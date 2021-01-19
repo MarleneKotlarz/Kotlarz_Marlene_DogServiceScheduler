175a4b0fc22ac607f2438476effb1c838950636d
MKotlarz <33337266+MarleneKotlarz@users.noreply.github.com>
2021-01-18 19:03:07 -0500

    removed #36 in AppointmentViewModel = getAppointmentIdForService

--- app/src/main/java/com/kotlarz_marlene_dogservicescheduler/ViewModel/AppointmentViewModel.java
+++ app/src/main/java/com/kotlarz_marlene_dogservicescheduler/ViewModel/AppointmentViewModel.java
@@ -26,7 +26,6 @@ public class AppointmentViewModel extends AndroidViewModel {
     private LiveData<List<AppointmentAndServiceOption>> allApptServices;
     Integer getAppointmentIdForService;
 
-    // TODO remove getAppointmentIdForService
 
     // Constructor
     public AppointmentViewModel(@NonNull Application application) {
@@ -34,7 +33,6 @@ public class AppointmentViewModel extends AndroidViewModel {
         // Instantiate repository and allAppointments
         schedulerRepository = new SchedulerRepository(application);
         allAppointments = schedulerRepository.getAllAppointments();
-//        getAppointmentIdForService = schedulerRepository.getAppointmentIdForService();
         allApptServices = schedulerRepository.getAppointmentAndServiceOptions();
     }
 
